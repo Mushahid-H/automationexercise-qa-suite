35a7d87476014a08bce2b3d70a469ec812fcef05
Mushahid-H <mushahidhussain882@gmail.com>
2026-07-25 23:32:07 +0500

    Testcase 16 Place order, login before checkout

--- tests/ui/PlaceOrder.spec.ts
+++ tests/ui/PlaceOrder.spec.ts
@@ -1,6 +1,7 @@
 import test from '@playwright/test';
 import { AddToCartPage, } from '../../src/pages/AddToCartPage';
 import {PlaceOrder} from '../../src/pages/PlaceOrder';
+import {LoginPage} from '../../src/pages/LoginPage'
 test.describe('Place Order Functionality', () => {
     test('Register While Checkout', async ({ page }) => {
         const addToCartPage = new AddToCartPage(page);
@@ -26,6 +27,19 @@ test.describe('Place Order Functionality', () => {
         await placeOrder.addProductToCart();
         await placeOrder.placeOrder();
     });
+    test('Login before checkout',async ({page})=>{
+        const addToCartPage = new AddToCartPage(page);
+        const placeOrder = new PlaceOrder(page);
+        const loginPage=new LoginPage(page);
+        await addToCartPage.goto();
+        await addToCartPage.ensureHomePage();
+        await placeOrder.clickLogin();
+        await loginPage.loginUser('test@piwi.com','test2123'); // do create acc first manually
+        await loginPage.ensureUserLoggedIn();
+        await placeOrder.addProductToCart();
+        await placeOrder.placeOrder();
+
+    })
 
 });
 
